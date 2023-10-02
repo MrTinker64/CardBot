b70120f5ba3ec0f74b92ab06c9eeb4c093165495
MrTinker64 <avi.micah@gmail.com>
2023-10-02 13:13:15 -0700

    Created a play command

--- bot.py
+++ bot.py
@@ -2,7 +2,9 @@
 
 import discord
 from discord.ext import commands
-from Deck import Card
+from Deck import Card, Deck, Suits
+from Hearts import HeartsGame
+from Player import Player
 from secretkey import TOKEN
 
 intents = discord.Intents.default()
@@ -20,6 +22,19 @@ async def on_ready():
 async def card(ctx, rank, of, suit):
     card = Card(suit, rank)
     await ctx.send(card)
+    
+@bot.command()
+async def hearts(ctx, p1, p2, p3, p4):
+    game = HeartsGame([p1, p2, p3, p4])
+    game.run_game()
+    await ctx.send("Game started!")
+    
+@bot.command()
+async def play(ctx, rank, of, suit):
+    p1 = Player("alice")
+    deck = Deck()
+    p1.receive_cards(deck.draw(1))
+    await ctx.send(f"{p1.name} played: {p1.play_card(rank, suit)}")
     
 # TODO Send point values at end of round
 
