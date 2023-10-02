bc331280d5dfb0a677802be58031a6f70d8aaf4e
MrTinker64 <avi.micah@gmail.com>
2023-10-02 09:24:00 -0700

    Made a card command

--- bot.py
+++ bot.py
@@ -2,13 +2,13 @@
 
 import discord
 from discord.ext import commands
-import random
+from Deck import Card
 from secretkey import TOKEN
 
 intents = discord.Intents.default()
 intents.message_content = True
 
-bot = commands.Bot(command_prefix='?', intents=intents)
+bot = commands.Bot(command_prefix='!', intents=intents)
 
 @bot.event
 async def on_ready():
@@ -17,34 +17,8 @@ async def on_ready():
 
 
 @bot.command()
-async def add(ctx, left: int, right: int):
-    """Adds two numbers together."""
-    await ctx.send(left + right)
-
-
-@bot.command()
-async def roll(ctx, dice: str):
-    """Rolls a dice in NdN format."""
-    try:
-        rolls, limit = map(int, dice.split('d'))
-    except Exception:
-        await ctx.send('Format has to be in NdN!')
-        return
-
-    result = ', '.join(str(random.randint(1, limit)) for r in range(rolls))
-    await ctx.send(result)
-
-
-@bot.command(description='For when you wanna settle the score some other way')
-async def choose(ctx, *choices: str):
-    """Chooses between multiple choices."""
-    await ctx.send(random.choice(choices))
-
-
-@bot.command()
-async def repeat(ctx, times: int, content='repeating...'):
-    """Repeats a message multiple times."""
-    for i in range(times):
-        await ctx.send(content)
+async def card(ctx, rank, of, suit):
+    card = Card(suit, rank)
+    await ctx.send(card)
 
 bot.run(TOKEN)
